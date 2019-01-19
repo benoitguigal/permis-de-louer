3c70ebd866abe6d697248f9cb715dc3b8263e2fa
amiceli <antoine@miceli.click>
2019-01-19 17:59:32 +0100

    Add dynamic component for reminder.

--- app/src/views/Start.vue
+++ app/src/views/Start.vue
@@ -9,12 +9,19 @@
                 <h2>
                     Vos informations
                 </h2>
+
+                <component v-bind:is="currentTabComponent"></component>
+            </div>
+            <div class="center-part">
+                <div class="wrapper">
+                    <h2>
+                        Votre logement
+                    </h2>
+                    <InformationStepper></InformationStepper>
+                </div>
             </div>
             <div class="right-part">
-                <h2>
-                    Votre logement
-                </h2>
-                <InformationStepper></InformationStepper>
+                <Stepper></Stepper>
             </div>
         </div>
     </div>
@@ -22,42 +29,68 @@
 
 <script>
     import InformationStepper from '@/components/InformationStepper'
+    import Stepper from '@/components/Stepper'
+    import ReminderStore from '@/store/ReminderStore'
 
     export default {
         name : 'start',
 
-        components : {InformationStepper},
+        components : {InformationStepper, Stepper},
+
+        store : ReminderStore,
 
         mounted () {
             document.body.scrollTop            = 0
             document.documentElement.scrollTop = 0
+        },
+
+        computed : {
+            currentTabComponent () {
+                return this.$store.state.currentTabComponent
+            }
         }
     }
 </script>
 
 <style lang="scss" scoped>
-    @mixin flexPart () {
+    @mixin flexPart() {
         h2 {
             text-align: center;
         }
     }
 
     .start {
+
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+
         .content {
             display: flex;
+            height: 100%;
 
             .left-part {
                 @include flexPart();
 
                 background: #ddd;
                 flex: 1;
+                overflow: hidden;
+            }
+
+            .center-part {
+                @include flexPart();
+
+                background: #eeeeee;
+                flex: 2;
+                height: 98%;
+                overflow: scroll;
             }
 
             .right-part {
                 @include flexPart();
 
-                background: #eeeeee;
-                flex: 2;
+                flex: 0 0 100px;
+                height: 100%;
             }
         }
     }
